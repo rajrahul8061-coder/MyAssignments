15ebf1ef2f4c14f8ccdc94805e93b4b2dcaf783c
rajrahul8061-coder <rajrahul8061@gmail.com>
2026-07-08 12:40:16 +0530

    Add files via upload
    Week 1 - Day 1(Javascripts basics code) - let, Var and Const variables declarations

---
+++ Day_1/javascriptbasics.js
@@ -0,0 +1,24 @@
+
+// Declare variables using let
+
+let firstName = "Rahul";
+let companyName = "OpenAI";
+let mobileNumber = 9876543210;
+let isAutomation = true;
+let hasPlaywright; // Uninitialized variable
+
+// Print values
+
+console.log("First Name:", firstName);
+console.log("Company Name:", companyName);
+console.log("Mobile Number:", mobileNumber);
+console.log("Is Automation:", isAutomation);
+console.log("Has Playwright:", hasPlaywright);
+
+// Print data types
+
+console.log("Type of firstName:", typeof firstName);
+console.log("Type of companyName:", typeof companyName);
+console.log("Type of mobileNumber:", typeof mobileNumber);
+console.log("Type of isAutomation:", typeof isAutomation);
+console.log("Type of hasPlaywright:", typeof hasPlaywright);
