c4a017aec5d0cc024fa126846a5f285469e6cf3e
rajrahul8061-coder <rajrahul8061@gmail.com>
2026-07-13 14:13:34 +0530

    Add files via upload

---
+++ Day 3/OddEvenNumbers.js
@@ -0,0 +1,11 @@
+
+//Write a JavaScript function named printOddNumbers that prints odd numbers between 1 and 25//
+    for(let i=1;i<=25;i++){
+if(i%2==1) //odd numbers and even numbers give 0
+
+console.log(i);
+
+    }
+
+
+
