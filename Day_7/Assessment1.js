7851dc836b47676111a7266fec1e40ae3118bd23
rajrahul8061-coder <rajrahul8061@gmail.com>
2026-07-15 20:26:00 +0530

    Add files via upload

---
+++ Day_7/Assessment1.js
@@ -0,0 +1,9 @@
+let num = [56, 78, 90, 23, 90, 76, 43, 56];
+
+for (let i = 0; i < num.length; i++) {
+    for (let j = i + 1; j < num.length; j++) {
+        if (num[i] === num[j]) {
+            console.log(num[i]);
+        }
+    }
+}
